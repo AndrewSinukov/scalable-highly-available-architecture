40f74e7637cb65c05b4a7cd2f94e14afc41a620d
AndrewSinukov <andrew.sinukov.it@gmail.com>
2022-01-07 17:42:56 +0200

    reafactoring: renamed layers name

--- application/sg.tf
+++ application/sg.tf
@@ -34,7 +34,7 @@ resource "aws_security_group" "ec2_private_security_group" {
     from_port = 0
     protocol  = "-1"
     to_port   = 0
-    cidr_blocks = [
+    security_groups = [
       aws_security_group.ec2_public_security_group.id
     ]
   }
